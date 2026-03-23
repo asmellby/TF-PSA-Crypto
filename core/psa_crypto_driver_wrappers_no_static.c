7cb465df29f0983b31e598f43b6cd9733a9f0605
Aksel Skauge Mellbye <aksel.mellbye@silabs.com>
2026-03-23 15:20:36 +0100

    core: Get key buffer size for PSA_KEY_LOCATION_LOCAL_STORAGE
    Support PSA_KEY_LOCATION_LOCAL_STORAGE in
    psa_driver_wrapper_get_key_buffer_size()
    
    Upstream-status: pending
    Signed-off-by: Aksel Skauge Mellbye <aksel.mellbye@silabs.com>

--- core/psa_crypto_driver_wrappers_no_static.c
+++ core/psa_crypto_driver_wrappers_no_static.c
@@ -84,6 +84,7 @@ psa_status_t psa_driver_wrapper_get_key_buffer_size(
     psa_key_location_t location = PSA_KEY_LIFETIME_GET_LOCATION( psa_get_key_lifetime(attributes) );
     psa_key_type_t key_type = psa_get_key_type(attributes);
     size_t key_bits = psa_get_key_bits(attributes);
+    size_t buffer_size = 0;
 
     *key_buffer_size = 0;
     switch( location )
@@ -105,6 +106,14 @@ psa_status_t psa_driver_wrapper_get_key_buffer_size(
             return( ( *key_buffer_size != 0 ) ?
                     PSA_SUCCESS : PSA_ERROR_NOT_SUPPORTED );
 #endif /* PSA_CRYPTO_DRIVER_TEST */
+
+        case PSA_KEY_LOCATION_LOCAL_STORAGE:
+            buffer_size = PSA_EXPORT_KEY_OUTPUT_SIZE( key_type, key_bits );
+            if( buffer_size == 0 ||
+                ( PSA_KEY_TYPE_IS_ECC_PUBLIC_KEY(key_type) && buffer_size == 1 ) )
+                return( PSA_ERROR_NOT_SUPPORTED );
+            *key_buffer_size = buffer_size;
+            return( PSA_SUCCESS );
 
         default:
             (void)key_type;
